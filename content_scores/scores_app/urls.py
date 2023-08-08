340b68dd53d666f232181e5e4e3bfa902f137d9a
deansainer <determind2k@gmail.com>
2023-08-08 12:23:06 +0200

    added session auth

--- content_scores/scores_app/urls.py
+++ content_scores/scores_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import *
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'Content', ContentViewSet)
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('movies/', views.movies_scores, name='movies_list'),
     path('series/', views.series_scores, name='series_list'),
-    path('games/', views.games_scores, name='games_list')
+    path('games/', views.games_scores, name='games_list'),
+    path('api/', include(router.urls)),
+    path('auth/', include('rest_framework.urls')),
 ]
--- content_scores/scores_app/views.py
+++ content_scores/scores_app/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import *
-
+from .serializers import *
 
 def home_page(request):
     context = {}
     return render(request, 'scores_app/home_page.html', context)
 def movies_scores(request):
-    movies_list = Content.objects.filter(content_type=2)
-    context = {'movies_list': movies_list}
+    movies_list = Content.objects.filter(content_type=2).order_by('score')[::-1]
+    list_length = len(movies_list)
+    context = {'movies_list': movies_list, 'list_length': list_length}
     return render(request, 'scores_app/movies_scores.html', context)
 
 def series_scores(request):
-    series_list = Content.objects.filter(content_type=3)
-    context = {'series_list': series_list}
+    series_list = Content.objects.filter(content_type=3).order_by('score')[::-1]
+    list_length = len(series_list)
+    context = {'series_list': series_list, 'list_length': list_length}
     return render(request, 'scores_app/series_scores.html', context)
 
 def games_scores(request):
-    games_list = Content.objects.filter(content_type=1)
-    context = {'games_list': games_list}
+    games_list = Content.objects.filter(content_type=1).order_by('score')[::-1]
+    list_length = Content.objects.filter(content_type=1).order_by('score')[::-1]
+    context = {'games_list': games_list, 'list_length': list_length}
     return render(request, 'scores_app/games_scores.html', context)
 
+class ContentViewSet(ModelViewSet):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
