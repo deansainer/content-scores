d96b8c06914e129eb12fa7cf1185c288b984c313
deansainer <determind2k@gmail.com>
2023-09-04 09:18:10 +0200

    changed rows width

--- content_scores/scores_app/views.py
+++ content_scores/scores_app/views.py
@@ -8,22 +8,20 @@ from .serializers import *
 def home_page(request):
     context = {}
     return render(request, 'scores_app/home_page.html', context)
+
 def movies_scores(request):
     movies_list = Content.objects.filter(content_type=2).order_by('score')[::-1]
-    list_length = len(movies_list)
-    context = {'movies_list': movies_list, 'list_length': list_length}
+    context = {'movies_list': movies_list}
     return render(request, 'scores_app/movies_scores.html', context)
 
 def series_scores(request):
     series_list = Content.objects.filter(content_type=3).order_by('score')[::-1]
-    list_length = len(series_list)
-    context = {'series_list': series_list, 'list_length': list_length}
+    context = {'series_list': series_list}
     return render(request, 'scores_app/series_scores.html', context)
 
 def games_scores(request):
     games_list = Content.objects.filter(content_type=1).order_by('score')[::-1]
-    list_length = Content.objects.filter(content_type=1).order_by('score')[::-1]
-    context = {'games_list': games_list, 'list_length': list_length}
+    context = {'games_list': games_list}
     return render(request, 'scores_app/games_scores.html', context)
 
 class ContentViewSet(ModelViewSet):
